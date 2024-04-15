14953a71ee7f76c0e0cf761f38aa1d214154131c
Electric Keet <ElectricKeet@users.noreply.github.com>
2024-04-15 10:59:21 -0700

    Font update.
    Still needs a bit of love to actually convert to code as needed.

--- res/make-iconfont.sh
+++ res/make-iconfont.sh
@@ -3,4 +3,4 @@
 
 echo "#include \"fonts.h\"" > ../src/gui/font_furicon.cpp
 # cat icons.ttf | zlib-flate -compress=9 | C:/Users/Georg/Downloads/xxd/xxd.exe -i -n "furIcons_compressed_data" | sed -r "s/^ +//g;s/, /,/g;s/ = /=/g;s/unsigned/const unsigned/g;s/compressed_data_len/compressed_size/" >> ../src/gui/font_furicon.cpp
-cat icons.ttf | zlib-flate -compress=9 | xxd -i -n "furIcons_compressed_data" | sed -r "s/^ +//g;s/, /,/g;s/ = /=/g;s/unsigned/const unsigned/g;s/compressed_data_len/compressed_size/" >> ../src/gui/font_furicon.cpp
+cat icons.ttf | zlib-flate -compress=9 | ./xxd -i -n "furIcons_compressed_data" | sed -r "s/^ +//g;s/, /,/g;s/ = /=/g;s/unsigned/const unsigned/g;s/compressed_data_len/compressed_size/" >> ../src/gui/font_furicon.cpp
