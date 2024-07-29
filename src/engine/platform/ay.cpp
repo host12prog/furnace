c3be52bcf26e4c091012760d366ed3d82a961d10
host12prog <hungnguyen.481335@gmail.com>
2024-07-29 17:42:11 +0700

    more magic numbers yay: a duplicate

--- src/engine/platform/ay.cpp
+++ src/engine/platform/ay.cpp
@@ -555,7 +555,7 @@ int DivPlatformAY8910::dispatch(DivCommand c) {
         chan[c.chan].sampleNote=DIV_NOTE_NULL;
         chan[c.chan].sampleNoteDelta=0;
         chan[c.chan].baseFreq=NOTE_PERIODIC(c.value);
-        chan[c.chan].tfx.period=(int)((rate / 16) / NOTE_PERIODIC(c.value));
+        chan[c.chan].tfx.period=(chan[i].freq+1);
         chan[c.chan].freqChanged=true;
         chan[c.chan].note=c.value;
       }
