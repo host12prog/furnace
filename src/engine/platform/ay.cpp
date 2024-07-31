7d671a588bbc0017b02d701cbd2dee8ea3ee9dda
host12prog <hungnguyen.481335@gmail.com>
2024-07-31 12:12:52 +0700

    i'm allergic to manual labour

--- src/engine/platform/ay.cpp
+++ src/engine/platform/ay.cpp
@@ -162,7 +162,7 @@ void DivPlatformAY8910::runTFX() {
       if (chan[i].tfx.counter >= chan[i].tfx.period && chan[i].tfx.mode == 0) {
         chan[i].tfx.counter = 0;
         chan[i].tfx.out ^= 1;
-        output = MAX(0,(((chan[i].tfx.out)?chan[i].outVol&15:chan[i].tfx.lowBound)-(15-chan[i].outVol)));
+        output = MAX(0, ((chan[i].tfx.out) ? (chan[i].outVol&15) : (chan[i].tfx.lowBound-(15-chan[i].outVol))));
         output &= 15;
         if (!isMuted[i]) {
           immWrite(0x08+i,output|(chan[i].curPSGMode.getEnvelope()<<2));
@@ -176,9 +176,6 @@ void DivPlatformAY8910::runTFX() {
       }
       if (chan[i].tfx.counter >= chan[i].tfx.period && chan[i].tfx.mode == 2) {
         chan[i].tfx.counter = 0;
-        if (!isMuted[i]) {
-          immWrite(0x08+i,(chan[i].outVol&15)|(chan[i].tfx.out<<5));
-        }
       }
       if (chan[i].tfx.mode == -1 && !isMuted[i]) {
         if (intellivision && chan[i].curPSGMode.getEnvelope()) {
